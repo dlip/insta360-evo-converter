4bfc5db8a2de88e5376e3b6e6c4679003ef20a49
Dane Lipscombe <danelipscombe@gmail.com>
2022-10-09 22:47:52 +1100

    Start video conversion

--- convert.ts
+++ convert.ts
@@ -1,23 +1,67 @@
-import { readableStreamToText } from "bun";
-import { spawn } from "bun";
+import { readdirSync } from "fs";
+import { exec } from "child_process";
 
-const proc = spawn({
-  cmd: ["ls", "-l"],
+function run(cmd: string) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error: unknown, stdout: string, stderr: string) => {
+      if (error) return reject(error);
+      if (stderr) return reject(stderr);
+      resolve(stdout);
+    });
+  });
+}
 
-  // Both of these forms work:
+async function convert() {
+  const inputPath = "./input";
+  const outputPath = "./output";
+  const donePath = "./done";
 
-  // as an array:
-  stdio: ["ignore", "pipe", "ignore"],
+  interface File {
+    fileName: string;
+  }
 
-  // You can also use "inherit" to inherit the parent's stdio.
-  // stdin: "inherit",
+  interface Vr1803d extends File {
+    kind: "vr-180-3d.mp4";
+    rightEyeFileName: string;
+  }
 
-  // You can pass a Bun.file to save it to a file:
-  // stdout: Bun.file("/tmp/stdout.txt"),
-});
+  interface Vr3602d extends File {
+    kind: "vr-360-2d.mp4";
+  }
 
-const result = await readableStreamToText(proc.stdout);
+  type VrFile = Vr1803d | Vr3602d;
 
-await proc.exited();
+  const fileNames = readdirSync(inputPath);
+  const vrFiles: Array<VrFile> = [];
 
-console.log(result);
+  for (let i = 0; i < fileNames.length; i++) {
+    const fileName = fileNames[i];
+    const vr1802dMatch = fileName.match(
+      /^VID_([0-9]*)_([0-9]*)_([0-9]*)_([0-9]*)\.insv$/
+    );
+    if (vr1802dMatch) {
+      if (vr1802dMatch[3] == "10") {
+        // Ignore right eye file and assume it always exists for now
+        continue;
+      }
+      vrFiles.push({
+        kind: "vr-180-3d.mp4",
+        fileName,
+        rightEyeFileName: fileName.replace("_00_", "_10_"),
+      });
+    }
+  }
+
+  for (let vrFile of vrFiles) {
+    console.log(vrFile);
+    if (vrFile.kind == "vr-180-3d.mp4") {
+      const outputFile = vrFile.fileName
+        .replace("_00_", "_")
+        .replace(".insv", ".mp4");
+      const command = `ffmpeg -i ${inputPath}/${vrFile.fileName} -i ${inputPath}/${vrFile.rightEyeFileName} -filter_complex hstack -c:v libx264 -crf 26 ${outputPath}/${outputFile}`;
+      await run(command);
+    }
+  }
+}
+
+convert();
